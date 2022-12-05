781aad0e51ea77ba72ad9f8bf75978fc25a06609
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-12-05 14:06:14 +0000

    completed empty cart

--- src/navBar/sideCart/SideCart.js
+++ src/navBar/sideCart/SideCart.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import "./SideCart.css"
-import cartIcon from "../shopping-cart-icon.svg"
+import { motion } from 'framer-motion'
+
 import CartItem from './components/CartItem'
 
+import cartIcon from "../shopping-cart-icon.svg"
+import stockImage from "../parachute.jpg"
+
 function SideCart({cart, setCart, openCart, setOpenCart}) {
+    
+    const buttonVariant= {
+        visible : {
+            scale: 1.1,
+            backgroundColor: "#000000",
+            color: "#FFFFFF",
+            transition: {
+                duration: 0.2, 
+            }
+        },
+      }
+
+    const closeModal = (e)=>{
+        if(e.currentTarget === e.target){
+            setOpenCart(!openCart)
+        }else{
+            return
+        }
+    }
+
     return (
-        <div className="sideCart__container">
+        <div 
+        className="sideCart__container"
+        onClick={(e)=>{closeModal(e)}}
+        >
             <div className="sideCart__box">
                 <header className="sideCart__header">
                 
@@ -30,34 +57,62 @@ function SideCart({cart, setCart, openCart, setOpenCart}) {
                     </div>
                 </header>
 
+                { cart.length > 0 ? (
+                    <>
+                        <main className="sideCart__items">
+                            { cart ?
+                                cart.map((item)=>{
+                                    return <CartItem key={item.id} item={item}/>
+                                })
+                                :
+                                null
+                            }
+                        </main>
+                        <div className="sideCart__promo-container">
+                            <p>enter promo code</p>
+                            <button>
+                                <p>+</p>
+                            </button>
 
-                <main className="sideCart__items">
-                    { cart ?
-                        cart.map((item)=>{
-                            return <CartItem key={item.id} item={item}/>
-                        })
-                        :
-                        null
-                    }
-                </main>
-                <div className="sideCart__promo-container">
-                    <p>enter promo code</p>
-                    <button>
-                        <p>+</p>
-                    </button>
+                        </div>
+                        <div className="sideCart__subTotal-container">
+                            <p>subtotal: </p>
+                        </div>
+                        <div className="sideCart__checkOut__button-container">
+                            <button className="sideCart__checkOut__button ">
+                                    <p>CHECKOUT</p>
+                            </button>
+                        </div>
+                        <div className="sideCart__message-container">
+                            <p>By clicking ‘CHECKOUT’, you will be redirected to the eShopWorld checkout page where payment will be taken and your order fulfilled.</p>
+                        </div>
+                    </>):(
+                        <div className="sideCart__empty-container">
+                            <div className="sideCart__empty__image-container">
+                                <img className="sideCart__empty__image" src={stockImage} />
+                            </div>
+                                <p className="sideCart__empty__message" >Your cart is empty...</p>
+                            <div className="sideCart__empty__button-container">
+                                <motion.button
+                                className="sideCart__empty__button"
+                                variants={buttonVariant}
+                                whileHover="visible"
+                                >
+                                    <p>Shop Mens</p>
+                                </motion.button>
 
-                </div>
-                <div className="sideCart__subTotal-container">
-                    <p>subtotal: </p>
-                </div>
-                <div className="sideCart__checkOut__button-container">
-                    <button className="sideCart__checkOut__button ">
-                            <p>CHECKOUT</p>
-                    </button>
-                </div>
-                <div className="sideCart__message-container">
-                    <p>By clicking ‘CHECKOUT’, you will be redirected to the eShopWorld checkout page where payment will be taken and your order fulfilled.</p>
-                </div>
+                                <motion.button
+                                className="sideCart__empty__button"
+                                variants={buttonVariant}
+                                whileHover="visible"
+                                >
+                                    <p>Shop Womens</p>
+                                </motion.button>
+                            </div>
+                            
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
