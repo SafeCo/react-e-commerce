14539254c3e5f8b15c899bffd87d88d650cfe176
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-12-14 16:57:41 +0000

    styled input in footer

--- src/globalComponents/footer/Footer.js
+++ src/globalComponents/footer/Footer.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import {useContext} from 'react'
+import { MediaContext } from '../../context/MediaContext'
 import './Footer.css'
 
 //social media icons
@@ -6,12 +7,16 @@ import facebookIcon from "./social-media-icons/facebook-icon.svg"
 import instagramIcon from "./social-media-icons/instagram-icon.svg"
 import twitterIcon from "./social-media-icons/twitter-icon.svg"
 import youtubeIcon from "./social-media-icons/youtube-icon.svg"
+import emailIcon from "./social-media-icons/email-icon.svg"
 
 function Footer() {
+    const { matches } = useContext(MediaContext)
+
     return (
         <footer className="footer__container" >
             <div className="footer__list-container">
-                <ul className="footer__firstCol colSpace">
+
+                <ul className="footer__socialMedia colSpace">
                     <li className="footer__logo">Simple.</li>
                     <li>simple@simplemail.com</li>
                     <ul className="footer__socials">
@@ -30,7 +35,7 @@ function Footer() {
                     </ul>
                 </ul>
 
-                <ul className="footer__secondCol colSpace">
+                <ul className="footer__shop colSpace">
                     <li>
                         <h4>Shop Simple</h4>
                     </li>
@@ -48,7 +53,7 @@ function Footer() {
                     </li>
                 </ul>
 
-                <ul className="footer__thirdCol colSpace">
+                <ul className="footer__service colSpace">
                     <li>
                         <h4>Customer service</h4>
                     </li>
@@ -75,7 +80,7 @@ function Footer() {
                     </li>
                 </ul>
 
-                <ul className="footer__fourthCol colSpace">
+                <ul className="footer__join colSpace">
                     <li>
                         <h4>Join Simple.</h4>
                     </li>
@@ -93,21 +98,41 @@ function Footer() {
                     </li>
                 </ul>
 
-                <ul className="footer__fifthCol colSpace">
-                    <li>
-                        <p>Sign up to our newsletter</p>
-                    </li>
-                    <li>
-                        <input 
-                        type="text" 
-                        placeholder="Enter your email address.." 
-                        />
-                        <button>
-                            <p>Sign Up</p>
-                        </button>
-                    </li>
-                </ul>    
+                {matches && 
+                    <ul className="footer__signUp colSpace">
+                        <li>
+                            <p>Sign up to our newsletter</p>
+                        </li>
+                        <li className="footer__input-container">
+                            <input 
+                            className="footer__input"
+                            type="text" 
+                            placeholder="Enter your email address.." 
+                            />
+                            <button className="footer__input-button">
+                                <img  src={emailIcon} className="footer__input-icon" alt="Email Icon"  />
+                            </button>
+                        </li>
+                    </ul>    
+                }
             </div>
+                {!matches && 
+                        <ul className="footer__signUp mobile colSpace">
+                            <li>
+                                <p>Sign up to our newsletter</p>
+                            </li>
+                            <li className="footer__input-container">
+                                <input 
+                                className="footer__input"
+                                type="text" 
+                                placeholder="Enter your email address.." 
+                                />
+                                <button className="footer__input-button">
+                                    <img  src={emailIcon} className="footer__input-icon" alt="Email Icon"  />
+                                </button>
+                            </li>
+                        </ul>    
+                    }
             <div className="footer__tC">
                 <p>Terms & Conditions | </p>
                 <p>Privacy Policy |</p>
