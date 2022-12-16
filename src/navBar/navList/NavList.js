34224348c9875a2ba539a12c32e719a7ebc2fc4b
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-12-16 15:48:59 +0000

    refactored navList

--- src/navBar/navList/NavList.js
+++ src/navBar/navList/NavList.js
@@ -1,80 +1,18 @@
-import { motion } from 'framer-motion';
 import "./NavList.css"
+import NavListItem from './NavListItem';
 
 
 function NavList() {
-
-    const itemVariant= {
-        hidden: {
-            borderBottom: "solid #FFFFFF 2px",
-            
-        },
-
-        visible: {
-            borderBottom: "solid #000000 2px",
-            transition:{
-                duration: 0.2
-            }
-        }
-    }
+    const navListItems = ["Clothes","Technology","Jewelry", "Essentials", "Sale" ]
 
     return (
         <div className="navList__container">
         <ul className="navList__box">
-            <motion.li 
-            className="navList__item"
-            variants={itemVariant}
-            initial="hidden"
-            whileHover="visible"                        
-            >
-                <button>
-                    Clothes
-                </button>
-            </motion.li>
-
-            <motion.li 
-            className="navList__item"
-            variants={itemVariant}
-            initial="hidden"
-            whileHover="visible"
-            >
-                <button>
-                    Technology
-                </button>
-            </motion.li>
-
-            <motion.li 
-            className="navList__item"
-            variants={itemVariant}
-            initial="hidden"
-            whileHover="visible"
-            >
-                <button>
-                    Jewelry
-                </button>
-            </motion.li>
-
-            <motion.li 
-            className="navList__item"
-            variants={itemVariant}
-            initial="hidden"
-            whileHover="visible"
-            >
-                <button>
-                    Essentials
-                </button>
-            </motion.li>
-            
-            <motion.li 
-            className="navList__item"
-            variants={itemVariant}
-            initial="hidden"
-            whileHover="visible"
-            >
-                <button>
-                    Sale
-                </button>
-            </motion.li>
+            {
+                navListItems.map((name)=>{
+                    return <NavListItem key={name} item={name} />
+                })
+            }
         </ul>
         </div>
     )
---
+++ src/navBar/navList/NavListItem.js
@@ -0,0 +1,32 @@
+import { motion } from "framer-motion"
+import { Link } from "react-router-dom"
+export default function NavListItem({item}) {
+    const itemVariant= {
+        hidden: {
+            borderBottom: "solid #FFFFFF 2px",
+            
+        },
+
+        visible: {
+            borderBottom: "solid #000000 2px",
+            transition:{
+                duration: 0.2
+            }
+        }
+    }
+
+    return (
+        <Link to={"/products"} style={{textDecoration: "none", color: "none"}}>
+            <motion.li 
+            className="navList__item"
+            variants={itemVariant}
+            initial="hidden"
+            whileHover="visible"                        
+            >
+                <button>
+                    {item}
+                </button>
+            </motion.li>
+        </Link>
+    )
+}
