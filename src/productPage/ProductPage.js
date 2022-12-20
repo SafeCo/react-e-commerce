4771d57076250ecc03eadb4846d9946deba91b91
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-12-20 17:05:22 +0000

    Added animation to productpage

--- src/productPage/ProductPage.js
+++ src/productPage/ProductPage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, useContext } from "react"
 import { useParams } from "react-router-dom"
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import { CartContext } from "../context/CartContext"
 
 
@@ -47,78 +47,85 @@ function ProductPage() {
 
     return (
         <div className="productPage__container">
-            { loaded ? 
-                (   
-                <div className="productPage__product-container">
-                    <div className="productPage__images__container">
-                        <ul className="productPage__sideImages__container">
+            <AnimatePresence>
+                { loaded ? 
+                    (   
+                    <motion.div 
+                        className="productPage__product-container"
+                        initial={{opacity: 0}}
+                        animate={{opacity: 1}}
+                    >
+                        <div className="productPage__images__container">
+                            <ul className="productPage__sideImages__container">
 
-                            <li className="productPage__sideImage-container">
-                                <img className="productPage__sideImage" src={tempImage} />
-                            </li>
+                                <li className="productPage__sideImage-container">
+                                    <img className="productPage__sideImage" src={tempImage} />
+                                </li>
 
-                            <li className="productPage__sideImage-container">
-                                <img className="productPage__sideImage" src={tempImage} />
-                            </li>
+                                <li className="productPage__sideImage-container">
+                                    <img className="productPage__sideImage" src={tempImage} />
+                                </li>
 
-                            <li className="productPage__sideImage-container">
-                                <img className="productPage__sideImage" src={tempImage} />
-                            </li>
-                            
-                            <li className="productPage__sideImage-container">
-                                <img className="productPage__sideImage" src={tempImage} />
-                            </li>
-                            
-                            <li className="productPage__sideImage-container">
-                                <img className="productPage__sideImage" src={tempImage} />
-                            </li>
+                                <li className="productPage__sideImage-container">
+                                    <img className="productPage__sideImage" src={tempImage} />
+                                </li>
+                                
+                                <li className="productPage__sideImage-container">
+                                    <img className="productPage__sideImage" src={tempImage} />
+                                </li>
+                                
+                                <li className="productPage__sideImage-container">
+                                    <img className="productPage__sideImage" src={tempImage} />
+                                </li>
 
-                            <li className="productPage__sideImage-container">
-                                <img className="productPage__sideImage" src={tempImage} />
-                            </li>
+                                <li className="productPage__sideImage-container">
+                                    <img className="productPage__sideImage" src={tempImage} />
+                                </li>
 
-                        </ul>
-                        <div className="productPage__mainImage-container">
-                            <img className="productPage__mainImage" src={product.image} />
-                        </div>
-                    </div>
-                    
-                    <div className="productPage__info__container">
-                        <div className="productPage__title-container container-spacing">
-                            <h2>{product.title}</h2>
-                        </div>
-
-                        <div className="productPage__rating-container container-spacing">
-                            <div className="productPage__rating-stars"> 
-                                <Rating rating={product.rating.rate}/>
-                                <div>{product.rating.rate}/5</div>
+                            </ul>
+                            <div className="productPage__mainImage-container">
+                                <img className="productPage__mainImage" src={product.image} />
                             </div>
-                            <p className="productPage__rating-review">{product.rating.count} reviews</p>
                         </div>
+                        
+                        <div className="productPage__info__container">
+                            <div className="productPage__title-container container-spacing">
+                                <h2>{product.title}</h2>
+                            </div>
 
-                        <div className="productPage__price-container container-spacing">
-                            <p>£{product.price.toFixed(2)}</p>
-                            <p>Tax and duties included</p>
+                            <div className="productPage__rating-container container-spacing">
+                                <div className="productPage__rating-stars"> 
+                                    <Rating rating={product.rating.rate}/>
+                                    <div>{product.rating.rate}/5</div>
+                                </div>
+                                <p className="productPage__rating-review">{product.rating.count} reviews</p>
+                            </div>
+
+                            <div className="productPage__price-container container-spacing">
+                                <p>£{product.price.toFixed(2)}</p>
+                                <p>Tax and duties included</p>
+                            </div>
+                            <div className="productPage__description-container container-spacing">
+                                <p>Description</p>
+                                <p>{product.description}</p>
+                            </div>
+                            <div className="productPage__button-container">
+                                <button 
+                                onClick={()=>{updateCart(product)}}
+                                className="productPage__button"
+                                >
+                                    ADD TO CART
+                                </button>
+                            </div>
                         </div>
-                        <div className="productPage__description-container container-spacing">
-                            <p>Description</p>
-                            <p>{product.description}</p>
-                        </div>
-                        <div className="productPage__button-container">
-                            <button 
-                            onClick={()=>{updateCart(product)}}
-                            className="productPage__button"
-                            >
-                                ADD TO CART
-                            </button>
-                        </div>
-                    </div>
-                </div>) 
-                :
-                (
-                <LoadingSpinner/>
-                )  
-            }
+                    </motion.div>) 
+                    :
+                    (
+                    <LoadingSpinner/>
+                    )  
+                }
+            </AnimatePresence>
+
             <Footer/>
         </div>
     )
